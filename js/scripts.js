36b5e32db4e30855ca481d1e6dbcc54cb41c0387
Mark <markmwago@gmail.com>
2019-03-06 08:48:47 +0300

    add css styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
-$(document).ready(function)
+$(document).ready(function(){
 
-$(button.ROLLDICE).click(function()) {
-  
+function genNumber(){
+return Math.floor(Math.random ()*6)+1;
 }
+  var initialScore=0;
+$("button#roll1").click(function (){
+  var anyNo=genNumber();
+  document.getElementById("die").innerHTML=anyNo;
+  var lastScore=initialScore+= anyNo;
+  document.getElementById("score1").innerHTML=lastScore;
+});
+var initScore=0;
+$("button#roll2").click(function (){
+var anyNo=genNumber();
+document.getElementById("die").innerHTML=anyNo;
+var lastScore=initialScore+= anyNo;
+document.getElementById("score2").innerHTML=lastScore;
+});
+});
